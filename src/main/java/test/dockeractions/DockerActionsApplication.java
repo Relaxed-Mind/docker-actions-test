d7c6061a9fadd3bc794f1805ed7e4b726292a32a
Relaxed-Mind <dlwlstn0709@uos.ac.kr>
2023-07-14 14:51:44 +0900

    init

---
+++ src/main/java/test/dockeractions/DockerActionsApplication.java
@@ -0,0 +1,13 @@
+package test.dockeractions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerActionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerActionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/test/dockeractions/DockerActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package test.dockeractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
